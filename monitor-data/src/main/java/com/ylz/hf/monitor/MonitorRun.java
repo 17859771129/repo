3365ad8390f5a328271145c80d0ad1b841770efd
17859771129 <a13799644143@163.com>
2020-11-07 18:53:30 +0800

    test

--- monitor-data/src/main/java/com/ylz/hf/monitor/MonitorRun.java
+++ monitor-data/src/main/java/com/ylz/hf/monitor/MonitorRun.java
@@ -1,5 +1,5 @@
 package com.ylz.hf.monitor;
-
+//test
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
